7069c2a1856a03b9bb8227d3bc04deb7890d1a17
Balu Manzano <jorgemanzanoweb@gmail.com>
2021-04-20 15:44:31 -0300

    axios call for localhost

--- src/containers/homeContainer.tsx
+++ src/containers/homeContainer.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from 'components/AppBar/AppBar';
+import axios from 'axios';
 
-const HomeContainer = (): JSX.Element => (
-  <>
-    <AppBar />
-  </>
-);
+const HomeContainer = (): JSX.Element => {
+  const [shipmentsData, setShipmentsData] = useState<Record<string, any>>({});
 
+  useEffect(() => {
+    axios.get('http://localhost:8080/data').then((response) => console.log(response));
+  },[]);
+  return (
+    <>
+      <AppBar />
+    </>
+  );
+}
 export default HomeContainer;
